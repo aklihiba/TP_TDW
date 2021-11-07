b337f72ca2e7d15fd4e5db7f08aaec4b21e1a4fe
akli hiba <ih_akli@esi.dz>
2021-11-07 14:46:05 +0100

    objectif 6 : insertion et calcule total done!

---
+++ script.js
@@ -0,0 +1,105 @@
+
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+    document.getElementById('form_tab2').style.display= "none";
+  }
+  
+function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+  }
+
+function select_tab(selectobj){
+    let v = selectobj.value;
+    t1 = document.getElementById("form_tab1");
+    t2 = document.getElementById("form_tab2");
+    if (v == "1"){
+        document.getElementById("form_tab2").style.display = "none";
+       document.getElementById("form_tab1").style.display = "block";
+    }
+    if(v == "2"){
+        document.getElementById("form_tab1").style.display = "none";
+        document.getElementById("form_tab2").style.display = "block";
+    }
+}  
+
+function add(){
+    let form =document.getElementById("form");
+    if (form.elements['0'].value == "1"){
+        culture = form.elements["culture"].value;
+        superficie = form.elements["superficie"].value;
+        production = form.elements["production"].value;
+
+        let tabl1 = document.getElementById("tab1");
+
+        let row = tabl1.insertRow(1);
+        let u = document.createElement('u');
+        let cell1 = document.createElement('th');
+        cell1.appendChild(u);
+        row.appendChild(cell1);
+        cell1.setAttribute("scope","Row")
+
+        let cell2 = row.insertCell(1);
+        let cell3 = row.insertCell(2);
+
+        u.innerHTML = culture;
+        cell2.innerHTML = superficie;
+        cell3.innerHTML = production;
+        let tot = parseFloat( document.getElementById('total0').innerText) ;
+        document.getElementById('total0').innerHTML = tot + parseFloat(superficie);
+        
+        tot = parseFloat( document.getElementById('total1').innerText) ;
+        document.getElementById('total1').innerHTML = tot + parseFloat(production);
+        
+    }else{
+        espece = form.elements["espece"].value;
+        nombre = form.elements["nombre"].value;
+        let tabl2 = document.getElementById("tab2");
+
+        let row = tabl2.insertRow(1);
+        let u = document.createElement('u');
+        let cell1 = document.createElement('th');
+        cell1.appendChild(u);
+        row.appendChild(cell1);
+        cell1.setAttribute("scope","Row")
+
+        let cell2 = row.insertCell(1);
+
+        u.innerHTML = espece;
+        cell2.innerHTML = nombre;
+        
+        let tot = parseFloat( document.getElementById('total2').innerText) ;
+        document.getElementById('total2').innerHTML = tot + parseFloat(nombre);
+    }
+    return false;
+   }
+/* 
+   function calcul(){
+    if( table.id=="tab1"){
+        let rows =  table.rows;
+        let sum1 = 0;
+        let sum2 = 0;
+        for(i=1 ; i< rows.length; i++){
+            let col = rows[i].cells;
+            sum1 =+ col[1].value;
+            if(col.length==3){
+                sum2 =+ col[2].value;
+            }
+        }
+        document.getElementById("total0").innerHTML = sum1;
+       document.getElementById("total1").innerHTML = sum2;
+    }   
+    if(table.id=="tab2"){
+        let rows =  table.rows;
+        let sum = 0;
+        for(i=1 ; i< rows.length; i++){
+            let col = rows[i].cells;
+            if(col.length==2){
+                 sum =+ col[1].value;  
+            }    
+        }
+        console.log(sum)
+        document.getElementById("total2").innerHTML = sum;
+      
+    }
+   }
+ */  
